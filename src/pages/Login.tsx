f08f240fd42ac11734c75622dc925371d69b23e4
Shashini <ssshashini21@gmail.com>
2024-10-03 22:59:47 +0530

    create login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,55 @@
-import { Grid, GridItem, Image } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
 import Header from "../components/Header";
-import LoginImage from '../assets/istockphoto-1495388784-612x612 1.png'
+import LoginImage from "../assets/istockphoto-1495388784-612x612 1.png";
+import LoginCard from "../components/LoginCard";
 
 const Login = () => {
   return (
     <>
+    
       <Grid
-        templateAreas={`"header"
-                    "main"
-                    "footer"`}
-        gridTemplateRows={"100px 1fr 30px"}
+        templateAreas={{
+          base:`"header "main"`,
+          lg:`"header" "main"`
+        }}
+        gridTemplateRows={"100px 1fr"}
         gridTemplateColumns={"1fr"}
+        height={'100vh'}
       >
-        <GridItem pl="2" bg="#F1B900" area={"header"}>
+        
+        <GridItem
+          pl="2"
+          bg="#F1B900"
+          area={"header"}
+          position="fixed"
+          top="0"
+          left="0"
+          width="100%"
+          zIndex="10"
+        >
           <Header />
         </GridItem>
-
-        <GridItem pl="2" area={"main"}>
-          <Image src={LoginImage}/> 
-        </GridItem>
-        <GridItem pl="2" bg="blue.300" area={"footer"}>
-          Footer
+       
+        <GridItem
+          pl="2"
+          area={"main"}
+          position={"fixed"}
+          left="0"
+          width="100%"
+          padding={"0"}
+          paddingTop={'100px'}
+        
+        >
+          <Image src={LoginImage} width={'100%'} height={'100vh'} objectFit={"cover"}/>
+          <Box
+            position={"absolute"}
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            zIndex="1"
+          >
+            <LoginCard />
+          </Box>
         </GridItem>
       </Grid>
     </>

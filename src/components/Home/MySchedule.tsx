d4cd52f950ae8a9eefa3a2aea5f4ff100507fc02
Shashini <ssshashini21@gmail.com>
2024-10-20 00:05:27 +0530

    change images

--- src/components/Home/MySchedule.tsx
+++ src/components/Home/MySchedule.tsx
@@ -17,21 +17,21 @@ const MySchedule = () => {
       name: "Bench press",
       sets: 3,
       reps: 10,
-      image: "https://anabolicaliens.com/cdn/shop/articles/5f19b4eff633a10684ef6193_wide-grip-lat-pulldown-anabolic-aliens_500x.png?v=1644918521",
+      image: "https://apilyfta.com/static/GymvisualPNG/00471101-Barbell-Incline-Bench-Press_Chest_small.png",
     },
     {
       date: "2024-10-10",
       name: "Squats",
       sets: 4,
       reps: 10,
-      image: "https://anabolicaliens.com/cdn/shop/articles/5f19b4eff633a10684ef6193_wide-grip-lat-pulldown-anabolic-aliens_500x.png?v=1644918521",
+      image: "https://lyfta.app/images/exercises/14361101.png",
     },
     {
       date: "2024-10-10",
       name: "Deadlifts",
       sets: 4,
       reps: 10,
-      image: "https://anabolicaliens.com/cdn/shop/articles/5f19b4eff633a10684ef6193_wide-grip-lat-pulldown-anabolic-aliens_500x.png?v=1644918521",
+      image: "https://kinxlearning.com/cdn/shop/files/exercise-10_1000x.jpg?v=1613154681",
     },
   ];
 

106a5a6298a0a2c57553d2dbea60e11497ad3a8b
Shashini <ssshashini21@gmail.com>
2024-10-25 20:07:03 +0530

    Create view more modal

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -1,7 +1,8 @@
-import { Grid, GridItem, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 import HomeHeader from "../components/Home/HomeHeader";
 import Footer from "../components/Footer";
 import ExerciseCard from "../components/Exercises/ExerciseCard";
+import HorizontalBar from "../components/HorizontalBar";
 
 const Exercises = () => {
   const exerciseData = [
@@ -11,6 +12,11 @@ const Exercises = () => {
       name: "Bench Press",
       description:
         "The bench press is performed by lying on a flat bench with feet firmly on the ground. Grip the barbell with hands slightly wider than shoulder-width apart, lower the bar to your chest, and press it upwards until your arms are fully extended. Repeat for the desired number of reps.",
+      type: "Strength",
+      bodyArea: "Chest",
+      exerciseLevel: "Intermediate",
+      category: "Compound",
+      equipments: "Barbell, Bench",
     },
     {
       image:
@@ -18,6 +24,11 @@ const Exercises = () => {
       name: "Bench Press",
       description:
         "The bench press is performed by lying on a flat bench with feet firmly on the ground. Grip the barbell with hands slightly wider than shoulder-width apart, lower the bar to your chest, and press it upwards until your arms are fully extended. Repeat for the desired number of reps.",
+      type: "Strength",
+      bodyArea: "Chest",
+      exerciseLevel: "Intermediate",
+      category: "Compound",
+      equipments: "Barbell, Bench",
     },
     {
       image:
@@ -25,6 +36,11 @@ const Exercises = () => {
       name: "Bench Press",
       description:
         "The bench press is performed by lying on a flat bench with feet firmly on the ground. Grip the barbell with hands slightly wider than shoulder-width apart, lower the bar to your chest, and press it upwards until your arms are fully extended. Repeat for the desired number of reps.",
+      type: "Strength",
+      bodyArea: "Chest",
+      exerciseLevel: "Intermediate",
+      category: "Compound",
+      equipments: "Barbell, Bench",
     },
     {
       image:
@@ -32,6 +48,11 @@ const Exercises = () => {
       name: "Bench Press",
       description:
         "The bench press is performed by lying on a flat bench with feet firmly on the ground. Grip the barbell with hands slightly wider than shoulder-width apart, lower the bar to your chest, and press it upwards until your arms are fully extended. Repeat for the desired number of reps.",
+      type: "Strength",
+      bodyArea: "Chest",
+      exerciseLevel: "Intermediate",
+      category: "Compound",
+      equipments: "Barbell, Bench",
     },
   ];
   return (
@@ -64,13 +85,41 @@ const Exercises = () => {
           bg="#fff"
         >
           <Text sx={titleText}>Exercises</Text>
-          <Stack width={'100%'} align={'center'} justify={'center'}>
-          <SimpleGrid spacing={10} columns={{ base: 1, md: 2, lg: 3 }} maxW={'90%'}>
-          {exerciseData.map(({image,name,description})=>(
-            <ExerciseCard image={image} name={name} description={description}/>
-          ))}
-          </SimpleGrid>
+          <Stack width={"100%"} align={"center"} justify={"center"}>
+            <SimpleGrid
+              spacing={10}
+              columns={{ base: 1, md: 2, lg: 3 }}
+              maxW={"90%"}
+            >
+              {exerciseData.map(
+                ({
+                  image,
+                  name,
+                  description,
+                  type,
+                  bodyArea,
+                  exerciseLevel,
+                  category,
+                  equipments,
+                }) => (
+                  <ExerciseCard
+                    key={name}
+                    image={image}
+                    name={name}
+                    description={description}
+                    type={type}
+                    bodyArea={bodyArea}
+                    exerciseLevel={exerciseLevel}
+                    category={category}
+                    equipments={equipments}
+                  />
+                )
+              )}
+            </SimpleGrid>
           </Stack>
+          <Box mt={{ base: 3, lg: 5 }} mb={{ base: 2, lg: 5 }}>
+            <HorizontalBar />
+          </Box>
         </GridItem>
         <GridItem pl="2" area={"footer"} left="0" width="100%" padding="0">
           <Footer />

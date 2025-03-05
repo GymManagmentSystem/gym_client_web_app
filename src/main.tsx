b667a973c64cd13b1e7e8753e8edf539a73538cc
Shashini <ssshashini21@gmail.com>
2025-03-05 22:52:33 +0530

    fetch all exercises

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import { RouterProvider } from 'react-router-dom'; // Ensure you are importing from 'react-router-dom'
-import router from './Routes.tsx'; // Adjust the path as necessary
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes.tsx';
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 
+const client = new QueryClient();
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <StrictMode>
     <ChakraProvider>
+      <QueryClientProvider client={client}>
       <RouterProvider router={router} />
+      </QueryClientProvider>
+
     </ChakraProvider>
   </StrictMode>,
 );
